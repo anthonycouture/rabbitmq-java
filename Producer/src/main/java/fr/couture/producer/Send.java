608847cdd340c96b7bf7f99e6d7d1be31a98df7b
Anthony Couture <antobox28@gmail.com>
2020-10-19 19:20:07 +0200

    add

--- Producer/src/main/java/fr/couture/producer/Send.java
+++ Producer/src/main/java/fr/couture/producer/Send.java
@@ -4,23 +4,29 @@ import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.Connection;
 
-import java.nio.charset.StandardCharsets;
+import java.io.IOException;
 
 public class Send {
     private final static String QUEUE_NAME = "hello";
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
         factory.setUsername("anthony");
         factory.setPassword("anthony");
         factory.setPort(8081);
-        try (Connection connection = factory.newConnection();
-             Channel channel =  connection.createChannel()) {
+        Connection connection = null;
+        try {
+            connection = factory.newConnection();
+            Channel channel =  connection.createChannel();
             channel.queueDeclare(QUEUE_NAME, false, false, false, null);
             String message = "Hello World!";
             channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
             System.out.println(" [x] Sent '" + message + "'");
+        } catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            connection.close();
         }
     }
 }
